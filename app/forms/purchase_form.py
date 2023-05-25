080cfdb44360dbc2f7d5df22de8fb1cb29ddb3ef
Jessie Baron <jessiebaron.political@gmail.com>
2023-05-25 10:15:42 -0400

    edits

--- app/forms/purchase_form.py
+++ app/forms/purchase_form.py
@@ -2,7 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, IntegerField
 from wtforms.validators import DataRequired, ValidationError
 
+def name_is_too_long(form, field):
+    name = field.name
+
+    if len(name) > 2000:
+        raise ValidationError("name is too long")
 
 class PurchaseForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired(), name_is_too_long])
     date_of_purchase = DateField("date of purchase", validators=[DataRequired()])
